242c3de47c91314c6aa5225544bf7928588bab8f
John Jago <johnjago@protonmail.com>
2020-05-02 09:41:58 -0500

    Implement loading of saved dogs

--- main.go
+++ main.go
@@ -3,30 +3,43 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
+// Dog is a data structure to represent a dog.
+type Dog struct {
+	Name  string
+	About []byte
+	// About is a byte slice rather than a string so that io libraries can use it.
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+// save is a method that, when called on a Dog, creates a new file with the dog's
+// name as the file name and the about as the contents of the file.
+func (p *Dog) save() error {
+	filename := p.Name + ".dog"
+	return ioutil.WriteFile(filename, p.About, 0600)
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
+// loadDog reads information from the file with the given name and returns a
+// pointer to a new Dog struct.
+func loadDog(name string) (*Dog, error) {
+	filename := name + ".dog"
+	about, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	return &Dog{Name: name, About: about}, nil
+}
+
+// dogHandler runs when we hit the "/dog/" endpoint on the HTTP server.
+func dogHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Path[len("/dog/"):]
+	dog, _ := loadDog(name)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", dog.Name, dog.About)
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/dog/", dogHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
